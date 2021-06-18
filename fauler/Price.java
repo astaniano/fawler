bef1b6dc415b898f928b2bdc3f03cd2f3311b0bb
alex <debryniuk@gmail.com>
2021-06-18 16:45:41 +0300

    refactored 13 getFrequentRenterPoints moved to Price classes

--- fauler/Price.java
+++ fauler/Price.java
@@ -4,6 +4,10 @@ abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
 class ChildrensPrice extends Price {
     int getPriceCode() {
@@ -24,6 +28,10 @@ class NewReleasePrice extends Price {
 
     double getCharge(int daysRented){
         return daysRented * 3;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
     }
 }
 class RegularPrice extends Price {
